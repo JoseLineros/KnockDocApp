fa10a3c2861ac9ecdb47cc1b069f34dca2457635
Jose <jose_2907@hotmail.com>
2021-09-24 18:42:59 -0500

    conexion con atlas

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URL = 'mongodb://localhost:27017/knockDoc';
+// const URL = 'mongodb://localhost:27017/knockDoc';
+const URL = 'mongodb+srv://admin:Admin123@cluster0.pyeql.mongodb.net/empresaDB?retryWrites=true&w=majority';
 
 mongoose
     .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
