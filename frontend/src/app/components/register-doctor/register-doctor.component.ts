c0e4b99005cb6d438eb6f57c9a2e7a985087ab13
Calos <perezcarlos1580@gmail.com>
2021-09-28 18:23:31 -0500

    [Footer - About - Contact]

--- frontend/src/app/components/register-doctor/register-doctor.component.ts
+++ frontend/src/app/components/register-doctor/register-doctor.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Doctor } from 'src/app/models/Doctor';
 import { DoctorService } from 'src/app/services/doctor/doctor.service';
 import { IpsService } from 'src/app/services/ips/ips.service';
+import { NgForm } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register-doctor',
@@ -9,16 +11,33 @@ import { IpsService } from 'src/app/services/ips/ips.service';
   styleUrls: ['./register-doctor.component.css'],
 })
 export class RegisterDoctorComponent implements OnInit {
-  constructor(public ipsService: IpsService, public doctorService: DoctorService) {}
+  constructor(
+    public ipsService: IpsService,
+    public doctorService: DoctorService
+  ) {}
 
   ngOnInit(): void {
     this.getAllIps();
   }
 
   getAllIps() {
-    this.ipsService.getAllIps().subscribe((res:any) => {
+    this.ipsService.getAllIps().subscribe((res: any) => {
       this.ipsService.ips = res;
-      console.log(res)
+      console.log(res);
+    });
+  }
+
+  createDoctor(doctor: NgForm) {
+    this.doctorService.createUser(doctor.value).subscribe((res) => {
+      // alert('Apartamento Creado');
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Doctor Guardado',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      console.log(res);
     });
   }
 }
