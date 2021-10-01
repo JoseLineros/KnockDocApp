c8b2435f6231f840364a1bd42e73d1665a11886f
Angello Gómez <r9angello@hotmail.com>
2021-09-30 19:10:05 -0500

    [userstable]

--- frontend/src/app/components/users/list-users/list-users.component.ts
+++ frontend/src/app/components/users/list-users/list-users.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserService } from 'src/app/services/user/user.service';
+import Swal from 'sweetalert2';
+import { User } from 'src/app/models/User';
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
@@ -22,7 +24,51 @@ export class ListUsersComponent implements OnInit {
       this.loading = false;
     });
   }
+  save(users: NgForm) {
+    if (users.value._id) {
+      // Actualizar el formulario
+      console.log(users.value);
+      this.userServices.updateUser(users.value).subscribe((res) => {
+        // alert('Usuario Actualizado');
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Datos Guardados',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        this.getAllUsers();
+        this.clean(users);
+      });
+    } else {
+      this.userServices.createUser(users.value).subscribe(
+        (res) => {
+          alert('usuario Creado');
+          this.getAllUsers();
+          console.log(res);
+          this.clean(users);
+        },
+        (error) => {
+          // alert('usuario ya existe');
+          Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'El usuario ya existe',
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      );
+    }
+  }
 
-  
+  clean(form?: NgForm) {}
+
+  fillFields(users: User) {
+    this.userServices.selectedUser = users;
+  }
+
+
+  deleteDoctor(_id: string) {}
 
 }
