de709888545c734649a54a0f9cb9d6f9f07c4425
oscargon12 <oscargon12@gmail.com>
2021-10-12 14:46:42 -0500

    Ajustes en dashboard y listado de doctores

--- frontend/src/app/components/users/my-doctors/my-doctors.component.ts
+++ frontend/src/app/components/users/my-doctors/my-doctors.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/User';
 import { MyDoctorsService } from 'src/app/services/my-doctors/my-doctors.service';
 import { AppoinmentService } from 'src/app/services/appoinment/appoinment.service';
-
-
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-my-doctors',
@@ -19,14 +18,14 @@ export class MyDoctorsComponent implements OnInit {
   doctor:any
  
 
-  constructor(public mydoctorsService: MyDoctorsService) { 
+  constructor(public mydoctorsService: MyDoctorsService, public appoinmentService: AppoinmentService, public userService: UserService,) { 
     this.doctor = ""
   }
   
   ngOnInit(): void {
-  
-
+    this.getAllAppoinment();  
     this.doctorhistory()
+    this.getAllDoctors()
   }
   
   doctorhistory(){
@@ -34,5 +33,19 @@ export class MyDoctorsComponent implements OnInit {
       this.doctor = res
       console.log(res)
     })
+  }
+
+  getAllAppoinment() {
+    this.appoinmentService.getAllAppoinment().subscribe((res: any) => {
+      this.appoinmentService.appoinments = res;
+      console.log(res);
+    });
+  }
+
+  getAllDoctors() {
+    this.userService.getAllDoctors().subscribe((res) => {
+      this.userService.doctors = res;
+      console.log(this.userService.doctors);
+    });
   }
 }
