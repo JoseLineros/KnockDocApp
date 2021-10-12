4350caf7c36db0fda4d8832d2c2fd11c4e641ab0
oscargon12 <oscargon12@gmail.com>
2021-10-12 12:18:23 -0500

    Graficas de barras en dashboard y ajustes en diseño

--- frontend/src/app/components/doctors/dashboard/dashboard.component.ts
+++ frontend/src/app/components/doctors/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
   }
+
+  /* Bar chart */
+  multi: any[] = [];
+
+  view: [number, number] = [1000, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = '2022';
+  showYAxisLabel = true;
+  yAxisLabel = 'No. de citas';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  single = [
+    {
+      "name": "Ene",
+      "value": 45
+    },
+    {
+      "name": "Feb",
+      "value": 40
+    },
+    {
+      "name": "Mar",
+      "value": 45
+    },
+    {
+      "name": "Abr",
+      "value": 37
+    }
+    ,
+    {
+      "name": "May",
+      "value": 47
+    },
+    {
+      "name": "Jun",
+      "value": 43
+    },
+    {
+      "name": "Jul",
+      "value": 50
+    },
+    {
+      "name": "Agt",
+      "value": 51
+    },
+    {
+      "name": "Sept",
+      "value": 54
+    },
+    {
+      "name": "Oct",
+      "value": 22
+    },
+    {
+      "name": "Nov",
+      "value": 0
+    },
+    {
+      "name": "Dic",
+      "value": 0
+    }
+  ];
+  
+  constructor() {
+    //Object.assign(this, { single })
+  }
+
+  onSelect(event: any) {
+    console.log(event);
+  }
+  /* Bar chart end */
 
 }
--- frontend/src/app/components/users/dashboard-users/dashboard-users.component.ts
+++ frontend/src/app/components/users/dashboard-users/dashboard-users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-dashboard-users',
@@ -7,9 +9,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardUsersComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
   }
+
+  /* Bar chart */
+  multi: any[] = [];
+
+  view: [number, number] = [1000, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = '2022';
+  showYAxisLabel = true;
+  yAxisLabel = 'No. de citas';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  single = [
+    {
+      "name": "Ene",
+      "value": 2
+    },
+    {
+      "name": "Feb",
+      "value": 1
+    },
+    {
+      "name": "Mar",
+      "value": 0
+    },
+    {
+      "name": "Abr",
+      "value": 3
+    }
+    ,
+    {
+      "name": "May",
+      "value": 1
+    },
+    {
+      "name": "Jun",
+      "value": 2
+    },
+    {
+      "name": "Jul",
+      "value": 0
+    },
+    {
+      "name": "Agt",
+      "value": 0
+    },
+    {
+      "name": "Sept",
+      "value": 3
+    },
+    {
+      "name": "Oct",
+      "value": 1
+    },
+    {
+      "name": "Nov",
+      "value": 0
+    },
+    {
+      "name": "Dic",
+      "value": 0
+    }
+  ];
+  
+  constructor() {
+    //Object.assign(this, { single })
+  }
+
+  onSelect(event: any) {
+    console.log(event);
+  }
+  /* Bar chart end */
 
 }
